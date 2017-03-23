1915d6cb789e5bf688b13ca8b741bf730a597cb8
ZHlambo <1509368098@qq.com>
2017-03-23 18:17:03 +0800

    ui-router 嵌套路由实现

--- platforms/android/assets/www/js/personal.js
+++ platforms/android/assets/www/js/personal.js
@@ -1,6 +1,8 @@
 var personalModule = app
 console.log(personalModule);
-personalModule.register.controller('personalCtrl',['$scope',function($scope){
+personalModule.register.controller('personalCtrl',['$scope','$rootScope',function($scope,$rootScope){
   console.log($scope);
   $scope.name = "aaaa"
+  $scope.user = $rootScope.user || {}
+  $scope.user.headerimg = $scope.user.headerimg || 'http://ok273wav2.qnssl.com/noPicture.png'
 }])
--- www/js/mall.js
+++ www/js/mall.js
@@ -1,6 +1,7 @@
 var mallModule = app
 console.log(mallModule);
-mallModule.register.controller('mallCtrl',['$scope',function($scope){
+mallModule.register.controller('mallCtrl',['$scope','$rootScope',function($scope,$rootScope){
   console.log($scope);
   $scope.name = "aaaa"
+  console.log($rootScope.user);
 }])
